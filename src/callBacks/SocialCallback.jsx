d65debb3b16d662abd3c4ee9f70081f6ac531453
naveedsarohani <naveed.sarohani@gmail.com>
2025-06-06 15:11:12 +0500

    Add notice for customer when making eSin cancellation

--- src/callBacks/SocialCallback.jsx
+++ src/callBacks/SocialCallback.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { showErrorToast } from "../utils/toast";
+import { showErrorToast, showSuccessToast } from "../utils/toast";
 import { setUserAndToken } from "../features/auth/authSlice";
 import { LoadingScreen } from "../components";
 
@@ -9,7 +9,45 @@ const SocialCallback = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  // useEffect(() => {
+  //   const handleCallback = async () => {
+  //     const urlParams = new URLSearchParams(window.location.search);
+
+  //     const error = urlParams.get("error");
+  //     if (error) {
+  //       showErrorToast(error);
+  //       navigate("/login");
+  //       return;
+  //     }
+
+  //     const token = urlParams.get("accessToken");
+  //     const userParam = urlParams.get("user");
+  //     let userObject = null;
+
+  //     if (userParam) {
+  //       try {
+  //         userObject = JSON.parse(userParam);
+  //       } catch (error) {
+  //         console.error("Error parsing user object:", error);
+  //       }
+  //     }
+
+  //     if (token && userObject) {
+  //       localStorage.setItem("token", token);
+  //       localStorage.setItem("user", JSON.stringify(userObject));
+
+  //       await dispatch(setUserAndToken({ user: userObject, token }));
+  //       navigate("/dashboard");
+  //     } else {
+  //       navigate("/login");
+  //     }
+  //   };
+
+  //   handleCallback();
+  // }, [navigate, dispatch]);
+
   useEffect(() => {
+    // Cleanup Facebook's weird #_=_ hash
     if (window.location.hash && window.location.hash === "#_=_") {
       window.history.replaceState(null, null, window.location.pathname + window.location.search);
     }
@@ -30,7 +68,7 @@ const SocialCallback = () => {
 
       if (userParam) {
         try {
-          userObject = JSON.parse(decodeURIComponent(userParam));
+          userObject = JSON.parse(userParam);
         } catch (error) {
           console.error("Error parsing user object:", error);
         }
@@ -40,22 +78,8 @@ const SocialCallback = () => {
         localStorage.setItem("token", token);
         localStorage.setItem("user", JSON.stringify(userObject));
 
-        await dispatch(setUserAndToken({ user: userObject, token }));
-        window.location.href = "/dashboard";
-        // let plan = null;
-        // try {
-        //   const planRaw = localStorage.getItem("purchasePending");
-        //   plan = planRaw ? JSON.parse(planRaw) : null;
-        // } catch (err) {
-        //   window.location.href = "/dashboard";
-        //   console.warn("Invalid purchasePending data", err);
-        // }
-
-        // if (plan !== null && userObject.accountType == 1) {
-        //   window.location.href = `/eSim-plans`;
-        // } else {
-        //   window.location.href = "/dashboard";
-        // }
+        dispatch(setUserAndToken({ user: userObject, token }));
+        navigate("/dashboard");
       } else {
         navigate("/login");
       }
